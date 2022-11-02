905778ff128a82080e03f4977a50d1221c6dd844
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-11-02 09:25:03 +0530

    added dummy code

---
+++ index.js
@@ -0,0 +1,12 @@
+const core= require('@actions/core');
+const github=require('@actions/githuib')
+try{
+    const name=core.getInput('who-to-greet');
+    console.log(`Hello ${name}`);
+    const time=(new Date()).toTimeString();
+    core.setOutput("time",time);
+    const payload=Json.strigify(github.context.payload,undefined,2);
+    console.log(`the event payload: ${payload}`);
+}catch(err){
+    core.setFailed(error.message);
+}
